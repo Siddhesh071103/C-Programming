661c2866d484da0d5261bae05227c8fa5c8cd6a2
Siddhesh071103 <103029308+Siddhesh071103@users.noreply.github.com>
2022-04-07 20:34:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main()
+{
+    int a = 2;
+    double const b = 4;
+    float c = 1.5;
+    char d = 'A';
+    printf("The value of a is %d\n",a);
+    printf("The value of b is %lf\n",b);
+    printf("The value of c is %.2f\n",c);
+    printf("The value of d is %c\n",d);
+    return 0;
+}
